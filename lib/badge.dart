5eabf8ba3999f459d5c9a87589c5384e4de5e10d
Haryan Dwi Baharna <haryandb@gmail.com>
2024-07-26 11:46:34 +0700

    badge text

--- lib/badge.dart
+++ lib/badge.dart
@@ -35,3 +35,38 @@ class Badge extends StatelessWidget {
     );
   }
 }
+
+class BadgeText extends StatelessWidget {
+  final String value;
+  final Color color;
+
+  const BadgeText({
+    required this.value,
+    required this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 40,
+      padding: const EdgeInsets.symmetric(vertical: 3, horizontal: 6),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            value,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+            ),
+            textAlign: TextAlign.center,
+          )
+        ],
+      ),
+    );
+  }
+}
