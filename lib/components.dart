873b062b260d809f11195ccc5ec2563810a40e36
Haryan Dwi Baharna <haryandb@gmail.com>
2024-08-15 09:27:13 +0700

    helpers spaan

--- lib/components.dart
+++ lib/components.dart
@@ -3,3 +3,4 @@ library components;
 export 'badge.dart';
 export 'my_dialog.dart';
 export 'forms.dart';
+export 'helpers.dart';
---
+++ lib/helpers.dart
@@ -0,0 +1,17 @@
+class Helpers {
+  static String sapan([DateTime? waktu]) {
+    waktu ??=
+        DateTime.now(); // Jika waktu tidak diberikan, gunakan waktu sekarang
+    int jam = waktu.hour;
+
+    if (jam >= 5 && jam < 12) {
+      return "Selamat Pagi!";
+    } else if (jam >= 12 && jam < 15) {
+      return "Selamat Siang!";
+    } else if (jam >= 15 && jam < 18) {
+      return "Selamat Sore!";
+    } else {
+      return "Selamat Malam!";
+    }
+  }
+}
